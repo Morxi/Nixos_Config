a3d262e2386d36356a7a3fa87e8cb70a00c99955
Moxi <git@morxi.com>
2021-10-11 09:22:36 +0800

    initize

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
+    home-manager.url = "github:nix-community/home-manager";
+    nixos-hardware.url = github:NixOS/nixos-hardware/master; 
+};
+  # 引入 nixos-cn flake 作为 inputs
+  inputs.nixos-cn = {
+    url = "github:nixos-cn/flakes";
+    # 强制 nixos-cn 和该 flake 使用相同版本的 nixpkgs
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { home-manager, nixpkgs, nixos-hardware, nixos-cn  , ... }: {
+    
+    nixosConfigurations = {
+      moxi-ga401 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+	  nixos-hardware.nixosModules.asus-zephyrus-ga401
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.moxi = ./moxi-home.nix;
+          }
+    	          ({ ... }: {
+            # 使用 nixos-cn flake 提供的包
+            environment.systemPackages =
+              [ nixos-cn.legacyPackages.x86_64-linux.netease-cloud-music ];
+            # 使用 nixos-cn 的 binary cache
+            nix.binaryCaches = [
+              "https://nixos-cn.cachix.org"
+            ];
+            nix.binaryCachePublicKeys = [ "nixos-cn.cachix.org-1:L0jEaL6w7kwQOPlLoCR3ADx+E3Q8SEFEcB9Jaibl0Xg=" ];
+
+            imports = [
+              # 将nixos-cn flake提供的registry添加到全局registry列表中
+              # 可在`nixos-rebuild switch`之后通过`nix registry list`查看
+              nixos-cn.nixosModules.nixos-cn-registries
+
+              # 引入nixos-cn flake提供的NixOS模块
+              nixos-cn.nixosModules.nixos-cn
+            ];
+          })
+        ];
+      };
+    };
+  };
+}
---
+++ moxi-home.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "moxi";
+  home.homeDirectory = "/home/moxi";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release
+  programs.bash = {
+    enable = true;
+        shellAliases = {
+ ll = "ls -l";
+  ".." = "cd ..";
+   gs="git status";
+   nix-add=''nix-env -iA --verbose --option  binary-caches https://mirrors.bfsu.edu.cn/nix-channels/store '';
+ };
+};
+
+  home.stateVersion = "21.05";
+}
