b08e0e002be66830561a1f0e4cc9f14ccb32568b
Moxi <git@morxi.com>
2021-10-24 22:27:25 +0800

    upd

--- mount-ga401-combine-disk.nix
+++ mount-ga401-combine-disk.nix
@@ -1,14 +1,17 @@
-{ lib, pkgs, config, ... }:
-with lib;      
+{ lib, pkgs, config, util-linux }:
+with lib;
 {
-  systemd.services.mount-win10-disks = {
-  script = ''
- losetup -f /home/moxi/windows-vmfs/efi1
- losetup -f  /home/moxi/windows-vmfs/efi2
- losetup -a
-  mdadm --build --verbose /dev/md0 --chunk=512 --level=linear --raid-devices=3 /dev/loop0 /dev/nvme0n1p2 /dev/loop1
 
-  '';
-  wantedBy = [ "multi-user.target" ];
-};
+  systemd.services.mount-win10-disks = {
+    path = with pkgs; [ mdadm ];
+
+    script = ''
+      ${util-linux}/bin/losetup -f /home/moxi/windows-vmfs/efi1
+      ${util-linux}/bin/losetup -f  /home/moxi/windows-vmfs/efi2
+      ${util-linux}/bin/losetup -a
+       mdadm --build --verbose /dev/md0 --chunk=512 --level=linear --raid-devices=3 /dev/loop0 /dev/nvme0n1p2 /dev/loop1
+
+    '';
+    wantedBy = [ "multi-user.target" ];
+  };
 }
