a7ab1285244153648f42310bdc42bea1727b501c
Moxi <git@morxi.com>
2021-10-25 19:11:03 +0800

    upd

--- moxi-modules/virtualisation.nix
+++ moxi-modules/virtualisation.nix
@@ -11,16 +11,23 @@ environment.systemPackages = with pkgs; [
 virtualisation.libvirtd = {
   enable = true;
   qemuOvmf = true;
-  qemuRunAsRoot = true;
   onBoot = "ignore";
   onShutdown = "shutdown";
+  qemuVerbatimConfig = ''
+        namespaces = []
+        nographics_allow_host_audio = 1
+        user = "moxi"
+        group = "kvm"
+      '';
 };
 
- 	
+ 	  boot.extraModprobeConfig = with boot.extraModprobeConfig;  ''
+    options kvm ignore_msrs=1
+  '';
 
-systemd.tmpfiles.rules = [
-  "f /dev/shm/looking-glass 0660 moxi qemu-libvirtd -"
-];
+# systemd.tmpfiles.rules = [
+#   "f /dev/shm/looking-glass 0660 moxi qemu-libvirtd -"
+# ];
 
 boot.postBootCommands = ''
     DEVS="0000:01:00.0 0000:01:00.1"
@@ -29,5 +36,8 @@ boot.postBootCommands = ''
       echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
     done
     modprobe -i vfio-pci
+    touch /dev/shm/looking-glass
+    chown moxi:kvm /dev/shm/looking-glass
+    chmod 660 /dev/shm/looking-glass
  '';
 }
